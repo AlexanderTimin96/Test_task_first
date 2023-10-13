de15cc664a27fd68a85006343a68104dcce5b621
Timin Alexander <timin.1996@list.ru>
2023-10-14 01:56:07 +0300

    Fix tests

--- src/test/java/repository/InMemoryRepositoryLeadersTest.java
+++ src/test/java/repository/InMemoryRepositoryLeadersTest.java
@@ -1,52 +1,51 @@
-//package repository;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.Arguments;
-//import org.junit.jupiter.params.provider.MethodSource;
-//import ru.tandemservice.repository.leadersboard.InMemoryRepositoryLeadersBoard;
-//
-//import java.util.LinkedHashMap;
-//import java.util.Map;
-//import java.util.stream.Stream;
-//
-//public class InMemoryRepositoryLeadersTest {
-//
-//    InMemoryRepositoryLeadersBoard repositoryLeaders = new InMemoryRepositoryLeadersBoard();
-//
-//    public static Stream<Arguments> argumentsStream() {
-//        return Stream.of(
-//                Arguments.of("Alex", 1),
-//                Arguments.of("Roma", 2),
-//                Arguments.of("Katya", 3),
-//                Arguments.of("Sergey", 4),
-//                Arguments.of("Anatoliy", 5),
-//                Arguments.of("Oleg", 6)
-//        );
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("argumentsStream")
-//    public void setPointsTest_NotThrowException(String nickname, int points) {
-//        Assertions.assertDoesNotThrow(() -> repositoryLeaders.setPoints(nickname, points));
-//    }
-//
-//
-//    @Test
-//    public void getLeaderboardTest() {
-//        repositoryLeaders.setPoints("Sergey", 4);
-//        repositoryLeaders.setPoints("Alex", 1);
-//        repositoryLeaders.setPoints("Oleg", 6);
-//        repositoryLeaders.setPoints("Anatoliy", 5);
-//
-//        Map<String, Integer> result = repositoryLeaders.getLeaderBoard(3);
-//
-//
-//        Map<String, Integer> expected = new LinkedHashMap<>();
-//        expected.put("Oleg", 6);
-//        expected.put("Anatoliy", 5);
-//        expected.put("Sergey", 4);
-//        Assertions.assertEquals(result, expected);
-//    }
-//}
+package repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.tandemservice.model.Player;
+import ru.tandemservice.model.Points;
+import ru.tandemservice.repository.leadersboard.InMemoryRepositoryLeadersBoard;
+import ru.tandemservice.repository.leadersboard.RepositoryLeadersBoard;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class InMemoryRepositoryLeadersTest {
+
+    RepositoryLeadersBoard repositoryLeaders = InMemoryRepositoryLeadersBoard.getInstance();
+
+    public static Stream<Arguments> argumentsStream() {
+        return Stream.of(
+                Arguments.of(new Player("test", "test@test.ru"), 0),
+                Arguments.of(new Player("test2", "test2@test.ru"), 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsStream")
+    public void setPointsTest_NotThrowException(Player player, int points) {
+        Assertions.assertDoesNotThrow(() -> repositoryLeaders.setPoints(player, points));
+    }
+
+
+    @Test
+    public void getLeaderboardTest() {
+        repositoryLeaders.setPoints(new Player("Sergey", "test@test1.ru"), 4);
+        repositoryLeaders.setPoints(new Player("Alex", "test@test2.ru"), 1);
+        repositoryLeaders.setPoints(new Player("Oleg", "test@test3.ru"), 6);
+        repositoryLeaders.setPoints(new Player("Anatoliy", "test@test4.ru"), 5);
+
+        Map<Player, Points> result = repositoryLeaders.getLeaderBoard(3);
+
+
+        Map<Player, Points> expected = new LinkedHashMap<>();
+        expected.put(new Player("Oleg", "test@test3.ru"), new Points(6));
+        expected.put(new Player("Anatoliy", "test@test4.ru"), new Points(5));
+        expected.put(new Player("Sergey", "test@test1.ru"), new Points(4));
+        Assertions.assertEquals(result.values().toString(), expected.values().toString());
+    }
+}
--- src/test/java/repository/InMemoryRepositoryPalindromeTest.java
+++ src/test/java/repository/InMemoryRepositoryPalindromeTest.java
@@ -1,36 +1,37 @@
-//package repository;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.Arguments;
-//import org.junit.jupiter.params.provider.MethodSource;
-//import ru.tandemservice.repository.palindrome.InMemoryRepositoryPalindrome;
-//
-//import java.util.stream.Stream;
-//
-//public class InMemoryRepositoryPalindromeTest {
-//
-//    InMemoryRepositoryPalindrome repositoryPalindrome = new InMemoryRepositoryPalindrome();
-//
-//    public static Stream<Arguments> argumentsStream() {
-//        return Stream.of(
-//                Arguments.of("Alex", "топот", true),
-//                Arguments.of("Sergey", "топот", true)
-//        );
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("argumentsStream")
-//    public void isValidPalindromeTest_true(String nickname, String palindrome, boolean expected) {
-//        boolean result = repositoryPalindrome.isValidPalindrome(nickname, palindrome);
-//        Assertions.assertEquals(expected, result);
-//    }
-//
-//    @Test
-//    public void isValidPalindromeTest_false() {
-//        repositoryPalindrome.isValidPalindrome("alex", "топот");
-//        boolean result = repositoryPalindrome.isValidPalindrome("alex", "топот");
-//        Assertions.assertFalse(result);
-//    }
-//}
+package repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.tandemservice.model.Player;
+import ru.tandemservice.repository.palindrome.InMemoryRepositoryPalindrome;
+
+import java.util.stream.Stream;
+
+public class InMemoryRepositoryPalindromeTest {
+
+    InMemoryRepositoryPalindrome repositoryPalindrome = new InMemoryRepositoryPalindrome();
+
+    public static Stream<Arguments> argumentsStream() {
+        return Stream.of(
+                Arguments.of(new Player("Alex", "test@test.ru"), "топот", true),
+                Arguments.of("Sergey", "топот", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsStream")
+    public void isValidPalindromeTest_true(Player player, String palindrome, boolean expected) {
+        boolean result = repositoryPalindrome.isValidPalindrome(player, palindrome);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void isValidPalindromeTest_false() {
+        repositoryPalindrome.isValidPalindrome(new Player("Alex", "test@test.ru"), "топот");
+        boolean result = repositoryPalindrome.isValidPalindrome(new Player("Alex", "test@test.ru"), "топот");
+        Assertions.assertFalse(result);
+    }
+}
